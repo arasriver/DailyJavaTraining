a57c891c7c457df2cbf1fdc471001d1ce471bc93
arasriver <aras.saeedian@gmail.com>
2025-08-10 14:55:12 +0200

    Some New Files

--- SingletonPattern.java
+++ SingletonPattern.java
@@ -1,2 +1,11 @@
-package DailyJavaTraining;public class SingletonPattern {
+package DailyJavaTraining;
+
+public class SingletonPattern {
+    private int value;
+    private static SingletonPattern obj = new SingletonPattern();
+    private SingletonPattern() {}
+    public static SingletonPattern getObjSingletonPattern() {
+        return obj;
+    }
+
 }
